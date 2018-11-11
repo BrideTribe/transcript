0a9c98be06275a9078a718ae562d3819a7bbe9b6
Akinnibosun Vicktor <ritymontero@gmail.com>
2018-11-11 20:24:52 +0100

    add form module and secret key

--- transcript.py
+++ transcript.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for 
+from forms import ApplicationForm, RecipientForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8b245eb98f56f7c909adbe0fada726b32e29a28f'
+app.config['']
 
 @app.route("/")
 @app.route("/home")
