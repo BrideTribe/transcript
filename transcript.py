facdcf200a23c87ab13a6f329574a12f6830e3ea
Akinnibosun Vicktor <ritymontero@gmail.com>
2018-11-13 14:20:04 +0100

    including other routes

--- transcript.py
+++ transcript.py
@@ -1,23 +1,41 @@
-from flask import Flask, render_template, url_for 
-from forms import ApplicationForm, RecipientForm
+from flask import Flask, render_template, url_for, request, flash
+from forms import GraduateApplicationForm, PostGraduateApplicationForm, GraduateRecipientForm, PostGraduateRecipientForm, ReturningGraduateUserForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '8b245eb98f56f7c909adbe0fada726b32e29a28f'
-app.config['']
+#app.config['']
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-@app.route("/graduate")
+@app.route("/graduate", methods=['POST', 'GET'])
 def graduate():
-    return render_template('graduate.html', title='Graduate')
+    form = GraduateApplicationForm()
+    form2 = ReturningGraduateUserForm()
+    
+    if form.validate_on_submit():
+        flash(f'Account created for {form.surname.data}')
+    return render_template('graduate.html', title='Graduate Application', form=form, form2=form2)
 
-@app.route("/postgraduate")
+@app.route("/postgraduate", methods=['POST', 'GET'])
 def postgraduate():
-    return render_template('postgraduate.html', title='PostGraduate')
+    form = PostGraduateApplicationForm()
 
+    return render_template('postgraduate.html', title='Post Graduate Application', form=form)
+
+@app.route("/grad_recipient", methods=['POST', 'GET'])
+def grad_recipient():
+    form = GraduateRecipientForm()
+
+    return render_template('recipient.html', title='Recipient Information', form=form)
+
+@app.route("/postgrad_recipient", methods=['POST', 'GET'])
+def postgrad_recipient():
+    form = PostGraduateRecipientForm()
+
+    return render_template('recipient.html', title='Recipient Information', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
